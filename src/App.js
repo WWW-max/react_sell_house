826edf46155d11393be6cc722c811967ab32b396
WWW-max <464711201@qq.com>
2022-10-04 21:26:21 +0800

    feat: 完成初始化配置

--- src/App.js
+++ src/App.js
@@ -1,10 +1,6 @@
-import { DatePicker } from 'antd';
-import { Button } from 'antd';
 function App() {
   return (
     <div className="App">
-      <DatePicker />
-      <Button type="primary">Primary Button</Button>
     </div>
   );
 }
